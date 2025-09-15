07af6f6310f86a1e5af6116b39e4c5a17db6ddc7
Levyyyy <Levi.moreau@edu.sait.ca>
2025-09-15 14:25:36 -0600

    finishing touches on code formatting

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Display() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week-2</Link>
+      <Link href="/week-2">Week-2 &#10140;</Link>
     </main>
   );
 }
--- src/app/week-2/student-info.js
+++ src/app/week-2/student-info.js
@@ -3,8 +3,11 @@ import Link from 'next/link';
 export default function StudentInfo() {
   return (
     <main>
-      <h1>Levi M</h1>
-      <Link href="https://github.com/Syntactix31/cprg306-assignments">Github Repo</Link>
+      <h1>Name: Levi M</h1>
+      <h2>
+        Github Repo: <Link href="https://github.com/Syntactix31/cprg306-assignments"><u>Syntactix31/cprg306-assignments</u></Link>
+      </h2>
+      
     </main>
   );
 }
