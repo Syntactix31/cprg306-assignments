6c4d619cecb9fc23d1b31642728db41d4839c265
Levyyyy <Levi.moreau@edu.sait.ca>
2025-09-15 14:12:49 -0600

    completed lab requirements but will add minor design and format tweaks later

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Page from "./week-2/page.js";
+import Link from 'next/link';
 
 export default function Display() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Page/>
+      <Link href="/week-2">Week-2</Link>
     </main>
   );
 }
--- src/app/week-2/student-info.js
+++ src/app/week-2/student-info.js
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+
 export default function StudentInfo() {
   return (
     <main>
       <h1>Levi M</h1>
-      <link href="">Github Repo</link>
+      <Link href="https://github.com/Syntactix31/cprg306-assignments">Github Repo</Link>
     </main>
   );
 }
