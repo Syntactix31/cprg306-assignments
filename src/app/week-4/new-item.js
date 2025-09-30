26c130e11889e1cd4623c211fb1c2d4dc661fb39
Levyyyy <Levi.moreau@edu.sait.ca>
2025-09-30 01:07:04 -0600

    Completion of increment and decrement buttons in new_item.js and fixed quantity formatting

--- src/app/week-4/new-item.js
+++ src/app/week-4/new-item.js
@@ -6,30 +6,37 @@ export default function NewItem() {
 
   const increment = () => {
     
-    if (setQuantity <= 20) {
+    if (quantity < 20) {
       setQuantity(quantity + 1);
     }
   };
 
   const decrement = () => {
     
-    if (setQuantity >= 1) {
+    if (quantity > 1) {
       setQuantity(quantity - 1);
     }
   };
 
 
   return (
-    <div>
-      <div className="font-bold text-4xl text-red-300 p-4 m-6 bg-white flex justify-center border-4 border-red-200">
-        <p>Quantity: {quantity}</p>
+    <section>
+      <div className="flex items-center justify-center font-bold text-4xl text-red-300 bg-white border-4 border-red-200 rounded-lg w-64 h-20 mx-auto my-10">
+        Quantity: {quantity}
       </div>
+
       <div className="flex justify-center gap-10">
       
-        <button onClick={decrement} className="bg-black py-4 px-12 my-4 rounded-lg text-white hover:bg-blue-900 cursor-pointer">Decrement</button>
-        <button onClick={increment} className="bg-red-700 py-4 px-12 my-4 rounded-lg text-white hover:bg-red-500 cursor-pointer">Increment</button>
+        <button disabled={quantity == 1} onClick={decrement} className={`bg-blue-500 py-4 px-12 my-4 rounded-lg text-white hover:bg-blue-900 cursor-pointer ${
+            quantity === 1 ? "opacity-50 cursor-not-allowed" : ""
+          }`}>Decrement</button>
+          
+          {/* hover:font-bold hover:border-red-300 hover:py-2 for clear change button hover:bg-white hover:border-4 hover:text-red-600*/}
+        <button disabled={quantity == 20} onClick={increment} className={`bg-red-500 py-4 px-12 my-4 rounded-lg text-white hover:bg-red-700  cursor-pointer ${
+            quantity === 20 ? "opacity-50 cursor-not-allowed" : ""
+          }`}>Increment</button>
       </div>
-    </div>
+    </section>
   );
 }
 
